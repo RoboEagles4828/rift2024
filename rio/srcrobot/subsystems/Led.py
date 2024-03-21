699ab7c7d77870d014370cfe2838b0d6ee21b36f
PratLyZol <pratkumar471@gmail.com>
2024-03-21 00:14:56 +0000

    LED Stuff - Nic

--- rio/srcrobot/subsystems/Led.py
+++ rio/srcrobot/subsystems/Led.py
@@ -1,6 +1,6 @@
 from commands2 import Subsystem
 import wpilib
-from wpilib import Spark
+from wpilib import PneumaticsControlModule, Solenoid
 from constants import Constants
 
 # This subsystem will continued to be developed as the season progresses
@@ -8,13 +8,58 @@ from constants import Constants
 class LED(Subsystem):
 
     def __init__(self):
-        self.kLedPort = 0
-        self.blinkin = Spark(self.kLedPort)
+        self.kLedPort = 22
+        self.pcm = PneumaticsControlModule(self.kLedPort)
+        self.pcm.disableCompressor() # only using for leds, so don't even use the compressor
 
-        self.RED = 0.61
-        self.PURPLE = 0.91
-        self.GREEN = 0.77
-        self.GOLD = 0.67
+        self.redChan = Solenoid(0, PneumaticsControlModule, 0)
+        self.greenChan = Solenoid(0, PneumaticsControlModule, 1)
+        self.blueChan = Solenoid(0, PneumaticsControlModule, 2)
+
+        self.BLACK = 0
+        self.OFF = 0
+        self.RED = 1
+        self.YELLOW = 2
+        self.GREEN = 3
+        self.TEAL = 4
+        self.BLUE = 5
+        self.PURPLE = 6
+        self.WHITE = 7
+
+    def set(self, color): # BLACK - 0, RED - 1, YELLOW - 2, GREEN - 3, TEAL - 4, BLUE - 5, PURPLE - 6, WHITE - 7
+        if color == 0: # BLACK / OFF
+            self.redChan.set(False)
+            self.greenChan.set(False)
+            self.blueChan.set(False)
+        elif color == 1: # RED
+            self.redChan.set(True)
+            self.greenChan.set(False)
+            self.blueChan.set(False)
+        elif color == 2: # YELLOW
+            self.redChan.set(True)
+            self.greenChan.set(True)
+            self.blueChan.set(False)
+        elif color == 3: # GREEN
+            self.redChan.set(False)
+            self.greenChan.set(True)
+            self.blueChan.set(False)
+        elif color == 4: # TEAL
+            self.redChan.set(False)
+            self.greenChan.set(True)
+            self.blueChan.set(True)
+        elif color == 5: # BLUE
+            self.redChan.set(False)
+            self.greenChan.set(False)
+            self.blueChan.set(True)
+        elif color == 6: # PURPLE
+            self.redChan.set(True)
+            self.greenChan.set(False)
+            self.blueChan.set(True)
+        elif color == 7: # WHITE / ON
+            self.redChan.set(True)
+            self.greenChan.set(True)
+            self.blueChan.set(True)
+
 
 # Red will be used when robot is on
     def idle(self):
